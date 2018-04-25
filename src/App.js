2f324607c52f9b4ea4522b2dc11ccc980d3473f4
yaskovdev <yaskovdev@gmail.com>
2018-04-25 20:25:53 +0300

    Add basic functionality.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,40 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    state = {
+        salary: '',
+        total: ''
+    };
+
+    render() {
+        const {salary, total} = this.state;
+        return (
+            <div>
+                <div>
+                    <input placeholder="Enter your net monthly salary" value={salary}
+                           onChange={this.handleChange}/>
+                    <input type="button" value="Start" onClick={this.handleStart}/>
+                </div>
+                <div>You have earned {total} &euro; so far</div>
+            </div>
+        );
+    }
+
+    handleChange = (event) => {
+        this.setState({salary: event.target.value})
+    };
+
+    handleStart = () => {
+        const {salary} = this.state;
+        const delta = salary / (30 * 24 * 3600);
+        const divider = 5;
+        let sum = 0;
+        setInterval(() => {
+            sum += delta / divider;
+            this.setState({total: sum.toLocaleString(undefined, {minimumFractionDigits: 4})});
+        }, 1000 / divider);
+    }
 }
 
 export default App;
---
+++ src/SalaryInput/SalaryInput.js
@@ -0,0 +1,8 @@
+import React, {Component} from 'react';
+
+export class SalaryInput extends Component {
+    render = () => {
+        return <div>
+        </div>;
+    }
+}
